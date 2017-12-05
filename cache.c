afc6554b8bd5904f84fde8c1bdb80c1ae6675a5b
Andrew C Tate <atate2@kingscanyon.cs.byu.edu>
2017-12-04 19:14:41 -0700

    Adding linked list cache

--- cache.c
+++ cache.c
@@ -4,41 +4,66 @@
 #define MAX_OBJECT_SIZE 102400
 #define CACHE_NUMBER 5
 
-typedef struct {
-  char path[10000];
-  unsigned char data[MAX_OBJECT_SIZE];
+typedef struct cache_obj {
+  char *url;
+  int size;
+  struct cache_obj *next;
+  unsigned char *data;
 } cache_obj ;
 
+cache_obj *HEAD;
+cache_obj *TAIL;
+int cache_size = 0;
 sem_t cache_sem;
-// Sem_init(&cache_sem, 0, 10);
 
-int cache_size = 5;
-cache_obj cache[CACHE_NUMBER];
+void cache_init() {
+  Sem_init(&cache_sem, 0, 10);
+}
 
-int cache_ptr = 0;
+void pop_head() {
+  if(!(HEAD->next)) {
+    return;
+  }
+  cache_size -= HEAD->size;
+  cache_obj *temp = HEAD;
+  HEAD = HEAD->next;
+  free(temp);
+}
 
-void cache_object(char* path, unsigned char *data, int data_size) {
+void cache_object(char* url, unsigned char *data, int data_size) {
+  if (data_size > MAX_OBJECT_SIZE) {
+    return;
+  }
   P(&cache_sem);
-  memset(cache[cache_ptr].path, 0, 10000);
-  memset(cache[cache_ptr].data, 0, MAX_OBJECT_SIZE);
-  strcpy(cache[cache_ptr].path, path);
-  memcpy(cache[cache_ptr].data, data, data_size);
-  cache_ptr = (cache_ptr + 1) % CACHE_NUMBER;
+  if (HEAD) {
+    TAIL->next = malloc(sizeof(cache_obj));
+    TAIL = TAIL->next;
+    TAIL->size = data_size;
+    TAIL->data = data;
+    TAIL->url = url;
+  }
+  else {
+    HEAD = TAIL = malloc(sizeof(cache_obj));
+    TAIL->size = data_size;
+    TAIL->data = data;
+    TAIL->url = url;
+  }
+  cache_size += data_size;
+  if (cache_size > MAX_CACHE_SIZE) {
+    pop_head();
+  }
   V(&cache_sem);
 }
 
-unsigned char *get_object(char *path) {
+unsigned char *cache_get_object(char *url) {
   printf("getting object!\n");
   P(&cache_sem);
-
-  unsigned char *ret = NULL;
-  for (int i = 0; i < CACHE_NUMBER; i++) {
-
-    if (!strcmp(cache[i].path, path)) {
-      ret = cache[i].data;
+  cache_obj *search = HEAD;
+  while (search) {
+    if (!strcmp(search->url, url)) {
+      return search->data;
     }
   }
-
   V(&cache_sem);
-  return ret;
+  return NULL;
 }
--- cache.h
+++ cache.h
@@ -3,7 +3,11 @@
 
 #include "csapp.h"
 
+sem_t cache_sem;
+int cache_size;int cache_ptr;
+
+void cache_init();
 void cache_object(char* path, unsigned char *data, int data_size);
-unsigned char *get_object(char *path);
+unsigned char *cache_get_object(char *path);
 
 #endif
